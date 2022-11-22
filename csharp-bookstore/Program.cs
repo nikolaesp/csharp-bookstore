32ae2327e39dc884dee356663036050d43c74e10
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-22 14:16:20 +0100

    fatto variabili

--- csharp-bookstore/Program.cs
+++ csharp-bookstore/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/* --------------- IL LIBRO DI OGGI: <<titolo>> di <<autore>> ----------------
+
+
+Informazioni generiche:
+ISBN: << isbn >>
+Numero delle pagine: << numeroPagine >> pagine
+Peso del libro: << pesoLibro >> kg
+Dimensioni del libro: << larghezza >> cm x << lunghezza >> cm x << profondita >> cm
+Informazioni Amazon:
+Numero di recensioni: << numeroRecensioni >> recensioni
+Valutazione media: << valutazioneMedia >> stelline
+Kindle disponibile: Si o No
+Copertina flessibile disponibile: Si o No */
+
+string titolo = "Clean Code: A Handbook of Agile Software Craftsmanship";
+string autore = "Robert C. Martin";
+int pagine = 464;
+long ISBN = 9780132350884;
+double peso = 0.7983226;
+float larghezza = 17.78f;
+float lunghezza = 23.368f;
+float profondita = 2.794f;
+int numeroRecensioni = 4741;
+double valutazioneMedia = 4.7;
+bool kindle = true;
+bool copertina = true;
